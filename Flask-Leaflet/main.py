905b191ff12d54159f399e3408955f50359fc0e8
manjergon <manjergon@alum.us.es>
2023-04-05 16:37:47 +0200

    unnecessary code removed

--- Flask-Leaflet/main.py
+++ Flask-Leaflet/main.py
@@ -13,4 +13,4 @@ def root():
     return render_template('index.html',markers=markers )
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=25000, debug=True)
